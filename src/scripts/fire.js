1f7a25c9bdbb79dfaa903d6511546f79659eeeab
Anant Bahuguna <anantbahugunaddn@gmail.com>
2020-05-26 10:34:34 +0530

    Update fire.js
    Remove API key

--- src/scripts/fire.js
+++ src/scripts/fire.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyCxpgf4mNoWSpZ80dAWbd9svd7nrpJnIgw",
-  authDomain: "chat-app-e2738.firebaseapp.com",
-  databaseURL: "https://chat-app-e2738.firebaseio.com",
-  projectId: "chat-app-e2738",
-  storageBucket: "chat-app-e2738.appspot.com",
-  messagingSenderId: "698868719117",
-  appId: "1:698868719117:web:ef82810e0d7ca5e6"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 var fire = firebase.initializeApp(config);
